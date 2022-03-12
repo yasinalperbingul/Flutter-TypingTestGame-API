9f34e36f2df141fc98d0ec335da37cc8299f2d9c
yasinalperbingul <55395610+yasinalperbingul@users.noreply.github.com>
2022-03-12 23:34:34 +0300

    server

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const users = [];
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(
+    users
+    .sort((user1,user2) => user1.score <= user2.score)
+    // .slice(0, 10)
+  );
+});
+
+router.post('/score', function(req, res, next) {
+  const {score,username} = req.body;
+  users.push({score: Number(score),username})
+  res.send({success: true});
+});
+
+module.exports = router;
