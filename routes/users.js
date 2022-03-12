9c6ef87a3b64cdb6bc2804ec9765f7aa0efe6258
yasinalperbingul <55395610+yasinalperbingul@users.noreply.github.com>
2022-03-12 23:46:19 +0300

    update score

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,14 @@ const users = [];
 router.get('/', function(req, res, next) {
   res.send(
     users
-    .sort((user1,user2) => user1.score <= user2.score)
-    // .slice(0, 10)
+    .sort((user1,user2) => {
+      if(user1.score < user2.score){
+        return 1;
+      }
+      if(user1.score > user2.score){
+        return -1;
+      }
+    }).slice(0, 10)
   );
 });
 
